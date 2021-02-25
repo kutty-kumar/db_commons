7dcd144f176ff99ba0e9ee2183a9b728bfd8c4a6
Kumar D <kumar.d@halodoc.com>
2021-02-25 10:54:37 +0530

    fix for multi get by external ids

--- model/base_repo.go
+++ model/base_repo.go
@@ -3,7 +3,7 @@ package db_commons
 type BaseRepository interface {
 	GetById(id uint64, creator EntityCreator) (error, Base)
 	GetByExternalId(externalId string, creator EntityCreator) (error, Base)
-	MultiGetByExternalId(externalIds[] string, creator func() []Base) (error, []Base)
+	MultiGetByExternalId(externalIds[] string, creator func() (string, []*Base)) (error, []*Base)
 	Create(base Base, externalIdSetter ExternalIdSetter) (error, Base)
 	Update(externalId string, updatedBase Base, creator EntityCreator) (error, Base)
 	Search(params map[string]string, creator EntityCreator) (error, []Base)
